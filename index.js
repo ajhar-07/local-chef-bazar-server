ed902bb4953b5687285098859f2e6a85cc3d80f0
ajhar-07 <fahimajhar737@gmail.com>
2025-12-09 13:54:54 +0600

    new Route Added

--- index.js
+++ index.js
@@ -32,10 +32,31 @@ const userCollection=db.collection('users')
 
 app.post('/users', async(req,res)=>{
     const userData=req.body
+    const query={email:userData.email}
+    userData.created_At=new Date().toISOString()
+    userData.Last_LogIn=new Date().toISOString()
+    userData.role="User"
+    userData.status="Active"
+    const AlreadyExists=await userCollection.findOne(query)
+    console.log(!!AlreadyExists);
+    if(AlreadyExists){
+        const result=userCollection.updateOne(query,{$set:{Last_LogIn:new Date().toISOString()}})
+        return res.send(result)
+    }
+    console.log("saving new user ...info");
+    
     const result=await userCollection.insertOne(userData)
     res.send(result)
 })
 
+
+//find user based on email
+
+app.get('/user/role/:email',async(req,res)=>{
+  const email=req.params.email
+  const result=await userCollection.findOne({email})
+  res.send({role:result?.role})
+})
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
